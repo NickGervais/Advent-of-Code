f17015b952892dbbe87b3a9cb1fb31183c96a0a8
Nicholas Gervais <nicholas.gervais@buzzfeed.com>
2019-12-11 23:17:44 -0600

    modify day 3

--- day3/solution.py
+++ day3/solution.py
@@ -3,26 +3,34 @@
 def split_line(line: str) -> (str, int):
     return line[:1], int(line[1:])
 
-def get_line_cordinates_set(origin: (int, int), direction: str, length: int) -> ((int, int), str):
+def get_line_cordinates_set(origin: (int, int), direction: str, length: int, val_cords: dict) -> ((int, int), dict):
     cur_x, cur_y = origin
     if direction == 'R':
-        return (cur_x + length, cur_y), set([f'{x}_{cur_y}' for x in range(cur_x, cur_x + length + 1)])
+        for x in range(cur_x, cur_x + length + 1):
+            val_cords[f'{x}_{cur_y}'] = len(val_cords)
+        return (cur_x + length, cur_y), val_cords
     elif direction == 'L':
-        return (cur_x - length, cur_y), set([f'{x}_{cur_y}' for x in range(cur_x - length, cur_x + 1)])
+        for x in range(cur_x - length, cur_x + 1):
+            val_cords[f'{x}_{cur_y}'] = len(val_cords)
+        return (cur_x - length, cur_y), val_cords
     elif direction == 'U':
-        return (cur_x, cur_y + length), set([f'{cur_x}_{y}' for y in range(cur_y, cur_y + length + 1)])
+        for y in range(cur_y, cur_y + length + 1):
+            val_cords[f'{cur_x}_{y}'] = len(val_cords)
+        return (cur_x, cur_y + length), val_cords
     elif direction == 'D':
-        return (cur_x, cur_y - length), set([f'{cur_x}_{y}' for y in range(cur_y - length, cur_y + 1)])
+        for y in range(cur_y - length, cur_y + 1):
+            val_cords[f'{cur_x}_{y}'] = len(val_cords)
+        return (cur_x, cur_y - length), val_cords
 
-def get_wire_cordinates_set(path: list) -> set:
-    cords = set()
+def get_wire_cordinates_map(path: list) -> set:
+    val_cords = {}
     cur_x = 0
     cur_y = 0
     for line in path:
         direction, length = split_line(line)
-        (cur_x, cur_y), new_cords = get_line_cordinates_set((cur_x, cur_y), direction, length)
-        cords.update(new_cords)
-    return cords
+        (cur_x, cur_y), new_val_cords = get_line_cordinates_set((cur_x, cur_y), direction, length, val_cords)
+        val_cords = new_val_cords
+    return val_cords
 
 def calc_manhattan_dist(x_1: int, y_1: int, x_2: int, y_2: int) -> int:
     x = abs(x_1 - x_2)
@@ -34,14 +42,21 @@ with open('input.txt', 'r') as input:
     for i, line in enumerate(input):
         paths.append([direction for direction in line.split(',')])
 
+## Part 1 ##
+
 path_1 = paths[0]
 path_2 = paths[1]
 
-cords_1 = get_wire_cordinates_set(path_1)
-cords_2 = get_wire_cordinates_set(path_2)
+val_cords_1 = get_wire_cordinates_map(path_1)
+val_cords_2 = get_wire_cordinates_map(path_2)
 
-intersections = cords_1.intersection(cords_2)
+cords_set_1 = set(val_cords_1.keys())
+cords_set_2 = set(val_cords_2.keys())
+
+
+intersections = cords_set_1.intersection(cords_set_2)
 intersections.discard('0_0')
+print(intersections)
 
 dists = []
 for cord in intersections:
@@ -53,3 +68,13 @@ for cord in intersections:
 print(min(dists))
 
 ## Part 2 ##
+
+shortest_combined_dist = float('inf')
+
+for cord in intersections:
+    dist = val_cords_1[cord] + val_cords_2[cord]
+    print(cord, dist)
+    if dist < shortest_combined_dist:
+        shortest_combined_dist = dist
+
+print(shortest_combined_dist)
