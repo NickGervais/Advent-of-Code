7083a0c706848f5579d8525e46e0f6643aa8366b
Nicholas Gervais <nicholas.gervais@buzzfeed.com>
2020-12-11 14:50:36 -0700

    day 7 solution

--- 2020/day7/solution.py
+++ 2020/day7/solution.py
@@ -30,22 +30,40 @@ with open('input.txt', 'r') as input:
         bags[color] = sub_bags
 
 
-def part_1():
+def part_1(bags):
     def has_color(target_color, cur_bag_color, bags):
         if cur_bag_color == target_color:
             return True
         else:
-            return any([has_color(target_color, bags[b[0]], bags) for b in bags[cur_bag_color]])
+            return any([has_color(target_color, b[0], bags) for b in bags[cur_bag_color]])
 
     
     target = 'shiny gold'
     valid_bags = set()
     for color, sub_bags in bags.items():
-        if has_color(target, color, bags):
+        if color != target and has_color(target, color, bags):
             valid_bags.add(color)
     
     return len(valid_bags)
 
-print(part_1())
+# print(part_1(bags))
+# solution: 278
 
+def part_2(bags):
+    def count_nested_bags(cur_color, bags, memo = {}):
+        if cur_color in memo:
+            return memo[cur_color]
+        
+        cur_bag_has_count = 0
+        for sub_bag in bags[cur_color]:
+            num_sub_bag = sub_bag[1]
+            cur_bag_has_count += num_sub_bag
+            for _ in range(num_sub_bag):
+                cur_bag_has_count += count_nested_bags(sub_bag[0], bags, memo)
+        memo[cur_color] = cur_bag_has_count
+        return cur_bag_has_count
 
+    print(count_nested_bags('shiny gold', bags))
+
+part_2(bags)
+# solution: 45157
