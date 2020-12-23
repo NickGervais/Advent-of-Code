b1618a443c0f6cc9b50b392b6c7c3e259df3382f
Nicholas Gervais <nicholas.gervais@buzzfeed.com>
2020-12-23 15:32:43 -0600

    day 22 partial

--- 2020/day22/solution.py
+++ 2020/day22/solution.py
@@ -25,19 +25,65 @@ def play_round(player_1, player_2):
         
     return player_1, player_2
 
+def calc_score(hand):
+    mult = len(hand)
+    score = 0
+    for i in hand:
+        score += mult * i
+        mult -= 1
+
+    return score
+
 def part_1(player_1, player_2):
     while 0 not in [len(player_1), len(player_2)]:
         player_1, player_2 = play_round(player_1, player_2)
 
     winner = player_1 if len(player_1) > 0 else player_2
 
-    mult = len(winner)
-    score = 0
-    for i in winner:
-        score += mult * i
-        mult -= 1
+    print(calc_score(winner))
 
-    print(score)
-
-part_1(player_1, player_2)
+# part_1(player_1, player_2)
 # solution: 35370
+
+def part_2(player_1, player_2):
+    def recursive_combat(p_1, p_2, game_num = 1):
+        round_num = 1
+        previous_plays = set()
+        while 0 not in [len(p_1), len(p_2)]:   
+            # print(f"-- Round {round_num} (Game {game_num}) --")  
+            # print(f"Player 1's deck: {', '.join([str(i) for i in p_1])}")
+            # print(f"Player 2's deck: {', '.join([str(i) for i in p_2])}")       
+            if f'{p_1[0]},{p_2[0]}' in previous_plays:
+                return 'p1', p_1, p_2  # game winner, p_1 hand, p_2 hand
+            
+            card_1, card_2 = p_1.pop(0), p_2.pop(0)
+            previous_plays.add(f'{card_1},{card_2}')
+            # print(f"Player 1 plays: {card_1}")
+            # print(f"Player 2 plays: {card_2}")
+
+            round_winner = None
+            if card_1 <= len(p_1) and card_2 <= len(p_2):
+                round_winner, sub_p_1, sub_p_2 = recursive_combat(p_1[:card_1], p_2[:card_2], game_num+1)
+            elif card_1 > card_2:
+                round_winner = 'p1'
+            elif card_2 > card_1:
+                round_winner = 'p2'
+
+            if round_winner == 'p1':
+                p_1.extend([card_1, card_2])
+            else:
+                p_2.extend([card_2, card_1])
+
+            # print(f"Round Winner: {round_winner}", f"p1: {', '.join([str(i) for i in p_1])}", f"p2: {', '.join([str(i) for i in p_2])}")
+            round_num += 1
+
+        game_winner = 'p1' if len(p_1) > 0 else 'p2'
+        # print(f"Game Winner: {game_winner}")
+        return game_winner, p_1, p_2
+
+    winner, p_1, p_2 = recursive_combat(player_1, player_2)
+    winner_hand = p_1 if winner == 'p1' else p_2
+    print(winner_hand)
+    print(calc_score(winner_hand))
+        
+part_2(player_1, player_2)
